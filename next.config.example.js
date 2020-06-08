5eb5ec06c2191aa9250ce9458daf7765dec12715
William L <w-lindvall@outlook.com>
2020-06-07 20:46:32 -0700

    move env vars to `.env`

--- next.config.example.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  serverRuntimeConfig: {
-    JWT_SECRET: 'kindred',
-    FIREBASE_KEY: 'mYaPPkeY',
-    FIREBASE_DOMAIN: 'my-app.firebaseapp.com',
-    FIREBASE_ID: 'my-app',
-  },
-}
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  serverRuntimeConfig: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    FIREBASE_KEY: process.env.FIREBASE_KEY,
+    FIREBASE_DOMAIN: process.env.FIREBASE_DOMAIN,
+    FIREBASE_ID: process.env.FIREBASE_ID,
+  },
+}
