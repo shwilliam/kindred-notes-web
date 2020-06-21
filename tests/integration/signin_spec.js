319b64ea22edc4dd13c95009348910ea8f03ed3a
William L <w-lindvall@outlook.com>
2020-06-20 17:47:31 -0700

    sketch sign in tests

---
+++ tests/integration/signin_spec.js
@@ -0,0 +1,43 @@
+import {doSignIn} from '../utils'
+
+// FIXME: expects user to exist
+const mockEmail = 'mail@mail.com'
+const mockPassword = 'password'
+
+describe('sign in', () => {
+  before(() => {
+    // ensure mock user account exists
+    cy.visit('http://localhost:3000/signup')
+
+    cy.get('[name="email"]').type(mockEmail).should('have.value', mockEmail)
+    cy.get('[name="password"]')
+      .type(mockPassword)
+      .should('have.value', mockPassword)
+
+    cy.get('[title="Create account"]').click()
+
+    cy.get('.error', {timeout: 20000})
+      .should('contain', 'email')
+      .should('contain', 'exists')
+
+    cy.visit('http://localhost:3000/signin')
+  })
+
+  it('can sign in', () => {
+    doSignIn(mockEmail, mockPassword)
+
+    // FIXME: check network response
+    cy.location('pathname').should('equal', '/')
+  })
+
+  it('can sign out', () => {
+    doSignIn(mockEmail, mockPassword)
+
+    cy.visit('http://localhost:3000/profile')
+
+    cy.get('[title="Sign out"]').click()
+
+    // FIXME: check network response
+    cy.location('pathname').should('equal', '/signout')
+  })
+})
--- tests/utils.js
+++ tests/utils.js
@@ -1,3 +1,15 @@
+export const doSignIn = (email, password) => {
+  cy.visit('http://localhost:3000/signin')
+
+  cy.get('[name="email"]').type(email).should('have.value', email)
+
+  cy.get('[name="password"]').type(password).should('have.value', password)
+
+  cy.get('[title="Log in"]').click()
+
+  cy.wait(5000)
+}
+
 // https://stackoverflow.com/questions/105034/how-to-create-guid-uuid
 export const generateId = (pattern = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx') =>
   pattern.replace(/[xy]/g, c => {
