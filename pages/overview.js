f59b5626f65024a287dfd5e81f29c24e8ffc3b8b
William L <w-lindvall@outlook.com>
2020-06-22 12:36:37 -0700

    create overview page

---
+++ pages/overview.js
@@ -0,0 +1,53 @@
+import {useQuery} from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import Link from 'next/link'
+import {withApollo} from '../apollo/client'
+import {FadeIn, Footer, Head, Header, Note, Spinner} from '../components'
+
+const Overview = () => {
+  const {data, loading} = useQuery(OverviewQuery)
+
+  return (
+    <main>
+      <Head title="Overview" description="Kindred Notes" />
+      <h1 className="sr-only">Kindred Notes</h1>
+      <Header />
+
+      {!loading && data ? (
+        <FadeIn className="footer-pad">
+          <h2 className="title -center">Recent Notes</h2>
+          <ul className="note-grid">
+            {data?.recentNotes.map(({id, content, color, style, font}) => (
+              <li className="note-grid__cell" key={id}>
+                <Link href={`/?note=${id}`} as={`/note/${id}`}>
+                  <a className="link -no-ul">
+                    <Note color={color} style={style} font={font}>
+                      {content}
+                    </Note>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </FadeIn>
+      ) : (
+        <Spinner full />
+      )}
+      <Footer />
+    </main>
+  )
+}
+
+const OverviewQuery = gql`
+  query OverviewQuery {
+    recentNotes {
+      id
+      content
+      color
+      font
+      style
+    }
+  }
+`
+
+export default withApollo(Overview)
