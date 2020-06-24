8cdb48365d99cea72e7fd48bb941d2db27a78b6e
William Lindvall <w-lindvall@outlook.com>
2020-06-23 18:26:06 -0700

    add link to login on home page if not authenticated

--- components/header.js
+++ components/header.js
@@ -1,7 +1,8 @@
 import {IconLogo} from './index'
 
-export const Header = () => (
+export const Header = ({children}) => (
   <div className="header" aria-hidden>
     <IconLogo className="header__logo" />
+    <div className="header__action">{children}</div>
   </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -11,69 +11,76 @@ const Index = () => {
     <main>
       <Head title="Kindred Notes" description="Kindred Notes" />
       <h1 className="sr-only">Kindred Notes</h1>
-      <Header />
+      <Header>
+        {!loading && !data?.viewer && (
+          <Link href="/signin">
+            <a className="link -no-ul">Sign in</a>
+          </Link>
+        )}
+      </Header>
 
       {!loading && data ? (
         <FadeIn className="footer-pad">
-          <>
-            <section className="wrapper">
-              <h2 className="sr-only">Overview</h2>
-              <ul className="overview-stats">
-                <li>
-                  <p className="overview-stats__item">
-                    <span className="overview-stats__data">
-                      {data.aggregateStatistics.totalUsers}
-                    </span>
-                    <span className="overview-stats__label">Users</span>
-                  </p>
-                </li>
-                <li>
-                  <p className="overview-stats__item">
-                    <span className="overview-stats__data">
-                      {data.aggregateStatistics.totalCountries}
-                    </span>
-                    <span className="overview-stats__label">Countries</span>
-                  </p>
-                </li>
-                <li>
-                  <p className="overview-stats__item">
-                    <span className="overview-stats__data">
-                      {data.aggregateStatistics.totalOpenedNotes}
-                    </span>
-                    <span className="overview-stats__label">Connections</span>
-                  </p>
-                </li>
-              </ul>
-            </section>
+          <section className="wrapper">
+            <h2 className="sr-only">Overview</h2>
+            <ul className="overview-stats">
+              <li>
+                <p className="overview-stats__item">
+                  <span className="overview-stats__data">
+                    {data.aggregateStatistics.totalUsers}
+                  </span>
+                  <span className="overview-stats__label">Users</span>
+                </p>
+              </li>
+              <li>
+                <p className="overview-stats__item">
+                  <span className="overview-stats__data">
+                    {data.aggregateStatistics.totalCountries}
+                  </span>
+                  <span className="overview-stats__label">Countries</span>
+                </p>
+              </li>
+              <li>
+                <p className="overview-stats__item">
+                  <span className="overview-stats__data">
+                    {data.aggregateStatistics.totalOpenedNotes}
+                  </span>
+                  <span className="overview-stats__label">Connections</span>
+                </p>
+              </li>
+            </ul>
+          </section>
 
-            <section>
-              <h2 className="title -center">Recent Notes</h2>
-              <ul className="note-grid">
-                {data.recentNotes?.map(({id, content, color, style, font}) => (
-                  <li className="note-grid__cell" key={id}>
-                    <Link href={`/note/${id}`}>
-                      <a className="link -no-ul">
-                        <Note color={color} style={style} font={font}>
-                          {content}
-                        </Note>
-                      </a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </section>
-          </>
+          <section>
+            <h2 className="title -center">Recent Notes</h2>
+            <ul className="note-grid">
+              {data.recentNotes?.map(({id, content, color, style, font}) => (
+                <li className="note-grid__cell" key={id}>
+                  <Link href={`/note/${id}`}>
+                    <a className="link -no-ul">
+                      <Note color={color} style={style} font={font}>
+                        {content}
+                      </Note>
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
         </FadeIn>
       ) : (
         <Spinner full />
       )}
-      <Footer />
+      {!loading && data?.viewer && <Footer />}
     </main>
   )
 }
 
 const HomeQuery = gql`
   query HomeQuery {
+    viewer {
+      id
+    }
     recentNotes {
       id
       content
