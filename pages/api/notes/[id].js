69c87419e87a1e0beb6ffd014ddcc3b268dd53bd
William Lindvall <w-lindvall@outlook.com>
2020-06-26 13:18:34 -0700

    create single note api route

---
+++ pages/api/notes/[id].js
@@ -0,0 +1,27 @@
+import {PrismaClient} from '@prisma/client'
+
+export default async (req, res) => {
+  const Prisma = new PrismaClient({log: ['query']})
+
+  try {
+    const {
+      query: {id},
+    } = req
+    const note = await Prisma.note.findOne({
+      where: {id: Number(id)},
+      include: {viewers: true},
+    })
+
+    if (note) {
+      res.json({note})
+    } else {
+      res.status(404)
+      res.json({error: {message: 'Note not found'}})
+    }
+  } catch (error) {
+    res.status(500)
+    res.json({error})
+  } finally {
+    await Prisma.disconnect()
+  }
+}
