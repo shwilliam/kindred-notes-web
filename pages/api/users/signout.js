e928e2a41f2087597dc4bec5b29082116fd492f1
William Lindvall <w-lindvall@outlook.com>
2020-06-26 23:56:11 -0700

    fix invalid sign out request type retval

--- pages/api/users/signout.js
+++ pages/api/users/signout.js
@@ -3,7 +3,7 @@ import {clearHeaderToken} from '../../../lib'
 export default async (_req, res) => {
   try {
     clearHeaderToken(res)
-    res.send({user: null})
+    res.json({user: null})
   } catch (error) {
     res.status(500)
     res.json({error})
