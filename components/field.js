093b6923faac517e470868cf26d1b844266ffc66
William Lindvall <w-lindvall@outlook.com>
2020-06-27 18:36:25 -0700

    style expandable notes seach

--- components/field.js
+++ components/field.js
@@ -1,37 +1,40 @@
-export const Field = ({
-  name,
-  label,
-  required = false,
-  autoComplete = 'false',
-  placeholder,
-  type = 'text',
-  center = false,
-  invert = false,
-  floating = false,
-  className,
-  value,
-  onChange
-}) => (
-  <div className={className}>
-    <label
-      id={[name, 'label'].join('-')}
-      htmlFor={[name, 'input'].join('-')}
-      className="sr-only"
-    >
-      {label} {required ? <span title="Required">*</span> : undefined}
-    </label>
-    <input
-      className={`input ${floating ? '-floating' : ''} ${
-        center ? '-center' : ''
-      } ${invert ? '-invert' : ''}`}
-      autoComplete={autoComplete}
-      id={[name, 'input'].join('-')}
-      name={name}
-      required={required}
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  </div>
+import {forwardRef} from 'react'
+
+export const Field = forwardRef(
+  (
+    {
+      label,
+      name,
+      required = false,
+      autoComplete = 'false',
+      type = 'text',
+      center = false,
+      invert = false,
+      floating = false,
+      className,
+      ...props
+    },
+    ref,
+  ) => (
+    <div className={className}>
+      <label
+        htmlFor={[name, 'input'].join('-')}
+        id={[name, 'label'].join('-')}
+        className="sr-only"
+      >
+        {label} {required ? <span title="Required">*</span> : undefined}
+      </label>
+      <input
+        name={name}
+        ref={ref}
+        autoComplete={autoComplete}
+        type={type}
+        id={[name, 'input'].join('-')}
+        className={`input ${floating ? '-floating' : ''} ${
+          center ? '-center' : ''
+        } ${invert ? '-invert' : ''}`}
+        {...props}
+      />
+    </div>
+  ),
 )
--- components/notes-search.js
+++ components/notes-search.js
@@ -1,18 +1,64 @@
+import {useRef, useState} from 'react'
 import {Field, IconSearch} from './index'
 
-export const NotesSearch = () => (
-  <form className="notes-search" action="/search" method="GET">
-    <Field
-      name="query"
-      type="search"
-      placeholder="Find a note..."
-      label="Notes search"
-      required
-    />
+export const NotesSearch = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleIsOpen = () => setIsOpen(s => !s)
+  const [inputValue, setInputValue] = useState('')
+  const toggleRef = useRef()
+  const inputRef = useRef()
 
-    <button className="button -floating" type="submit">
-      <span className="sr-only">Search</span>
-      <IconSearch className="icon -blue" aria-hidden />
-    </button>
-  </form>
-)
+  const handleInputChange = e => setInputValue(e.target.value)
+
+  const handleBlur = () => {
+    if (!inputRef.current?.value?.length) {
+      setIsOpen(false)
+      toggleRef.current?.focus()
+    }
+  }
+
+  const handleToggle = () => {
+    toggleIsOpen()
+    inputRef.current?.focus()
+  }
+
+  return (
+    <section className="notes-search">
+      <form
+        className={`notes-search__form ${isOpen ? '-open' : ''}`}
+        action="/search"
+        method="GET"
+      >
+        <Field
+          name="query"
+          type="search"
+          placeholder="Find a note..."
+          label="Notes search"
+          className="notes-search__input"
+          value={inputValue}
+          onChange={handleInputChange}
+          onBlur={handleBlur}
+          ref={inputRef}
+          required
+        />
+
+        <button className="button -floating notes-search__submit" type="submit">
+          <span className="sr-only">Search</span>
+          <IconSearch className="icon -blue" aria-hidden />
+        </button>
+      </form>
+
+      <button
+        onClick={handleToggle}
+        className={`button -floating notes-search__toggle ${
+          isOpen ? '-hidden' : ''
+        }`}
+        type="button"
+        aria-hidden
+        ref={toggleRef}
+      >
+        <IconSearch className="icon -blue" />
+      </button>
+    </section>
+  )
+}
