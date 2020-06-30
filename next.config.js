8368609d24a456683d1a7efabd8eb96721eac123
William Lindvall <w-lindvall@outlook.com>
2020-06-29 22:59:59 -0700

    set up code bundle analysis scripts

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   serverRuntimeConfig: {
     JWT_SECRET: process.env.JWT_SECRET,
     FIREBASE_KEY: process.env.FIREBASE_KEY,
     FIREBASE_DOMAIN: process.env.FIREBASE_DOMAIN,
     FIREBASE_ID: process.env.FIREBASE_ID,
   },
-}
+})
