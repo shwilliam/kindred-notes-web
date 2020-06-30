ebd91e1e18c6efe78bc4439b2050c5c8d2c9f49d
William Lindvall <w-lindvall@outlook.com>
2020-06-30 13:58:20 -0700

    add notifications link to nav

---
+++ components/icons/bell.js
@@ -0,0 +1,32 @@
+export const IconBell = ({fill = false, ...props}) =>
+  fill ? (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      {...props}
+    >
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+      <path strokeWidth="2" d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+    </svg>
+  ) : (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1"
+      strokeLinecap="round"
+      {...props}
+    >
+      <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+      <path strokeWidth="2" d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+    </svg>
+  )
