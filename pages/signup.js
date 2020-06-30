1473c8a4d146e89753d0bdfc3d1dae78386d6808
William Lindvall <w-lindvall@outlook.com>
2020-06-29 19:22:36 -0700

    use gdocs content for terms

--- pages/signup.js
+++ pages/signup.js
@@ -1,9 +1,16 @@
 import {AuthLayout, Head, SignupForm} from '../components'
+import {getGoogleDocCopy} from '../lib'
 
-export default () => (
+export default ({copy}) => (
   <AuthLayout>
     <Head title="Sign up" description="Sign up for a Kindred Notes account" />
     <h2 className="sr-only">Sign Up</h2>
-    <SignupForm />
+    <SignupForm termsAndConditionsCopy={copy.terms_and_conditions} />
   </AuthLayout>
 )
+
+export const getStaticProps = async () => {
+  const copy = await getGoogleDocCopy()
+
+  return {props: {copy}}
+}
