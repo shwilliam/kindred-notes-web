3e4990dc7a7fa404c83bb0366ff1be1a289727f6
William Lindvall <w-lindvall@outlook.com>
2020-07-03 12:54:27 -0700

    remove unused env vars

--- next.config.js
+++ next.config.js
@@ -5,8 +5,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   serverRuntimeConfig: {
     JWT_SECRET: process.env.JWT_SECRET,
-    FIREBASE_KEY: process.env.FIREBASE_KEY,
-    FIREBASE_DOMAIN: process.env.FIREBASE_DOMAIN,
-    FIREBASE_ID: process.env.FIREBASE_ID,
   },
 })
