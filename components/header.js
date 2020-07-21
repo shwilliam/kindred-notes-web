3ca372c6c5d8d0bb1af4fc6eb0d5dcdaffb79991
Roberto Hernandez <robhhr@outlook.com>
2020-07-20 18:28:08 -0700

    fix: update header layout (#17)
    Co-authored-by: William Lindvall <shwilliam@hey.com>

--- components/header.js
+++ components/header.js
@@ -3,14 +3,16 @@ import Link from 'next/link'
 
 export const Header = ({viewerId, children}) => (
   <div className="header">
-    <Link href="/">
-      <a className="nav__link">
-        <span className="sr-only">Home</span>
-        <IconLogo className="header__logo" aria-hidden />
-      </a>
-    </Link>
+    <div className="nav__container">
+      <Link href="/">
+        <a className="nav__link">
+          <span className="sr-only">Home</span>
+          <IconLogo className="header__logo" aria-hidden />
+        </a>
+      </Link>
 
-    <div className="header__action">{children}</div>
-    {viewerId ? <Nav viewerId={viewerId} /> : null}
+      <div className="header__action">{children}</div>
+      {viewerId ? <Nav viewerId={viewerId} /> : null}
+    </div>
   </div>
 )
