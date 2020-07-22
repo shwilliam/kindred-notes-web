5cda5d6a1d255e487a40f968467ad8bd0c4951bb
William Lindvall <shwilliam@hey.com>
2020-07-21 20:04:06 -0700

    fix: style note grid (#19)
    Co-authored-by: robhhr <robhhr@outlook.com>

--- components/note-grid.js
+++ components/note-grid.js
@@ -15,7 +15,7 @@ export const NoteGrid = ({
     setActiveVariant(s => (s === 'grid' ? 'list' : 'grid'))
 
   return (
-    <section className="wrapper">
+    <section className="wrapper -large">
       <h2 className="title -center">{title}</h2>
 
       {loading ? (
