19dc56c55f82cd1774a56f90c6ddcd1d7d91d40b
William Lindvall <shwilliam@hey.com>
2020-08-01 09:51:02 -0700

    move server runtime config vars to `.env`

--- next.config.js
+++ next.config.js
@@ -2,8 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer({
-  serverRuntimeConfig: {
-    JWT_SECRET: process.env.JWT_SECRET,
-  },
-})
+module.exports = withBundleAnalyzer({})
