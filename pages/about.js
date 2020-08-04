c688927deed6b314c18e0f488ac3b915b38a60a6
William Lindvall <shwilliam@hey.com>
2020-08-03 23:30:24 -0700

    update copy

---
+++ pages/about.js
@@ -0,0 +1,93 @@
+import Link from 'next/link'
+import {FadeIn, Footer, Head, Header, NotesSearch} from '../components'
+import {validateHeaderToken} from '../lib'
+
+export default ({viewerId}) => (
+  <main className="footer-pad">
+    <Head title="About kindrednotes" description="Connected Kindness" />
+    <Header viewerId={viewerId}>
+      {!viewerId ? (
+        <Link href="/signin">
+          <a className="link">Sign in</a>
+        </Link>
+      ) : (
+        <NotesSearch />
+      )}
+    </Header>
+
+    <FadeIn className="pad -bottom">
+      <section className="wrapper">
+        <h1 className="title -center">About</h1>
+        <p className="paragraph">
+          Hello! I’m Shelley, the founder of kindrednotes. Thank you for
+          stopping by to learn more about kindrednotes.
+        </p>
+        <p className="title -small">Let me ask you something...</p>
+        <ul className="check-list">
+          <li>Have you ever been the recipient of an act of kindness?</li>
+          <li>
+            Maybe a neighbour picked up your mail while you were away, or a
+            friend brought you flowers.
+          </li>
+          <li>
+            Perhaps a complete stranger bought you a cup of coffee or let you go
+            ahead in a long line up.
+          </li>
+          <li>
+            Maybe you’ve also done acts of kindness, like paying for someone’s
+            lunch, walking a neighbour’s dog, holding the elevator door or
+            complimenting a stranger.
+          </li>
+        </ul>
+
+        <p className="paragraph">
+          Sometimes we can get caught up in our own busy lives and forget that
+          the simplest things can have a large positive impact on others and
+          ourselves.
+        </p>
+
+        <p className="title -small -center -blue">
+          These seemingly small acts, matter ❤️
+        </p>
+
+        <p className="paragraph">
+          After working and traveling in over 50 countries, I've had the
+          opportunity to meet many people from all over the world and experience
+          the power of kindness. It's shown me that at the core, we all want the
+          same things: to be seen, to be heard and to be loved.
+        </p>
+
+        <br />
+
+        <p className="paragraph">
+          This is where the idea for kindrednotes was born.
+        </p>
+
+        <p className="title -small -center">
+          Our intention at kindrednotes is to connect people through kindness.
+          To demonstrate that despite our many differences in perspectives,
+          beliefs and values, at the core, we are all the same.
+        </p>
+
+        <p className="title -small -center -blue">Connected Kindness.</p>
+      </section>
+    </FadeIn>
+
+    {!viewerId && (
+      <div className="wrapper">
+        <Link href="/signup">
+          <a className="button -full">Join the movement</a>
+        </Link>
+      </div>
+    )}
+
+    <Footer />
+  </main>
+)
+
+export const getServerSideProps = ctx => {
+  const token = validateHeaderToken(ctx.req.headers)
+  const viewerId = token ? token.id : null
+
+  return {props: {viewerId}}
+}
--- pages/index.js
+++ pages/index.js
@@ -123,6 +123,127 @@ export default ({viewerId}) => {
               demonstrating that despite our many differences in perspectives,
               beliefs and values, that at the core, we are all the same.
             </p>
+
+            <h2 className="title -center">Connecting through kindness.</h2>
+
+            <p className="paragraph">
+              We act as a catalyst to inspire and create meaningful connections
+              and conversations with each other. We believe that we are all
+              connected and that by helping one person, we help all. It is our
+              “kindred” spirits that remind us that despite our many differences
+              in perspectives, beliefs and values, that at the core, we are all
+              the same.
+            </p>
+
+            <br />
+
+            <p className="paragraph">
+              So let’s start by sending notes to each other – all around the
+              world – have conversations, share, celebrate and bring more
+              positive energy into the world. It starts with You. One person at
+              a time, one kind note at a time. Together let’s create a community
+              that’s rooted in Kindness. Connected Kindness.
+            </p>
+
+            <br />
+
+            <div className="wrapper">
+              {!viewerId && (
+                <Link href="/signup">
+                  <a className="button -full">Join the movement</a>
+                </Link>
+              )}
+            </div>
+
+            <br />
+
+            <h2 className="title -center -blue">
+              Why Now: The Importance of kindrednotes
+            </h2>
+
+            <p className="paragraph">
+              <span className="bold">
+                Over 3B people are on social media every day, yet we are
+                currently experiencing a loneliness epidemic.
+              </span>{' '}
+              Mental health issues continue to increase with mental illness now
+              being the single largest disabling group of disorders worldwide,
+              affecting 1 in 4 people.
+            </p>
+
+            <p className="title -small -center">
+              Today, we have never been more connected, yet felt more
+              disconnected.
+            </p>
+
+            <p className="paragraph">
+              The pace of life is fast and only getting faster. However, there
+              is also a shift for something slower, more purposeful, more
+              intentional.
+            </p>
+
+            <p className="title -small -center">
+              Kindrednotes will provide the opportunity to connect and make a
+              positive impact on people around the world.
+            </p>
+
+            <ol className="fact-list">
+              <li>
+                <p className="title -small -blue">Fact #1: Kindness Heals.</p>
+                <p className="paragraph">
+                  Studies have shown that kind acts can lower blood pressure,
+                  pain, stress and depression while increasing self-esteem and
+                  optimism.
+                </p>
+              </li>
+              <li>
+                <p className="title -small -blue">
+                  Fact #2: Kindness is a WIN-WIN-WIN.
+                </p>
+                <p className="paragraph">
+                  Kindness not only benefits the recipient of the kindness but
+                  also the person doing the kind act and the people observing
+                  the kindness. Studies have shown that the person giving the
+                  kindness sometimes benefit as much as the recipient of the
+                  good deed. Kindness is teachable and compassion can be
+                  cultivated when people observe acts of kindness.
+                </p>
+              </li>
+              <li>
+                <p className="title -small -blue">
+                  Fact #3: Kindness is Contagious.
+                </p>
+                <p className="paragraph">
+                  Kindness can set off a chain reaction in a “pay it forward”
+                  domino effect, creating a conscious social movement that
+                  inspires others to engage and spread kindness.  By creating
+                  and sharing kindrednotes with each other all around the world,
+                  we will create a kindness movement.
+                </p>
+              </li>
+            </ol>
+
+            <p className="title -small -center">We Believe…</p>
+
+            <ul className>
+              <li>One person can make a huge difference.</li>
+              <li>Change starts with each one of us.</li>
+              <li>We are all connected. By helping one, we help all.</li>
+              <li>
+                Now more than ever we need to cultivate community, kindness and
+                empathy.
+              </li>
+              <li>Kindness is the cure.</li>
+            </ul>
+
+            <p className="title -small -center -blue">
+              Kindness starts with ONE
+            </p>
+
+            <p className="paragraph">
+              One person at a time, one kind note at a time. Together let’s
+              create a community that’s rooted in Kindness. Connected Kindness.
+            </p>
           </section>
         )}
 
@@ -135,14 +256,6 @@ export default ({viewerId}) => {
           />
         )}
       </FadeIn>
-
-      <div className="wrapper">
-        {!viewerId && (
-          <Link href="/signup">
-            <a className="button -full">Join the movement</a>
-          </Link>
-        )}
-      </div>
 
       <Footer />
     </main>
