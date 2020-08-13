218f9d58d269c30c8b83af0d68b99e80cbfba49d
William Lindvall <shwilliam@hey.com>
2020-08-13 12:39:51 -0700

    remove meta tags from `_document` `<Head>`

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Nunito:ital,wght@0,400;0,600;0,800;1,400&family=Permanent+Marker&display=swap"
             rel="stylesheet"
